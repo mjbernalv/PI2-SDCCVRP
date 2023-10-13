486d9f87e8cba7a05eb9e149846b5bb519630101
Maria Jose Bernal <71986858+mjbernalv@users.noreply.github.com>
2023-10-13 16:20:31 -0500

    Update

--- Function.py
+++ Function.py
@@ -9,44 +9,74 @@ import math, os, random
 #q: demands
 #d: distances
 
+# Reads a .txt with problem instances
 def readFile(path):
     txt = np.loadtxt(path)
-    Q=int(txt[0][0])
-    K=int(txt[0][1])
-    nodes=[]
-    q=[]
+    Q = int(txt[0][0])
+    K = int(txt[0][1])
+    nodes = []
+    q = []
     for node in txt[1:]:
         nodes.append([int(node[0]), int(node[1])])
         q.append(int(node[2]))
     return nodes, K, Q, q
 
+# Calculates the distance matrix for all nodes
 def distance(nodes):
-    distances=[]
+    distances = []
     for i in range(len(nodes)):
-        values=[]
+        values = []
         for j in range(len(nodes)):
-            distance=math.sqrt(math.pow(nodes[i][0]-nodes[j][0],2)+math.pow(nodes[i][1]-nodes[j][1],2))
+            distance = math.sqrt(math.pow(nodes[i][0] - nodes[j][0], 2) + math.pow(nodes[i][1] - nodes[j][1], 2))
             values.append(distance)
         distances.append(values)
     return distances
 
+# Returns a random sequence
 def sequence(N):
-    seq=[0]
-    order=np.random.permutation(N-1)
+    seq = [0]
+    order = np.random.permutation(N-1)
     for i in order:
         seq.append(i+1)
     return seq
 
-def cost(trip,Q,d,deliv): #Deliver
-    c=0
-    load=Q
+# Calculates the cost of a single trip
+def cost(trip,deliv,d): #Deliver -> sum(D_i*q_i)
+    c = 0
+    dist = 0
     for i in range(1,len(trip)):
-        c+=d[i-1][i]*load
-        load-=deliv[i]
+        dist += d[trip[i-1]][trip[i]]
+        c += dist * deliv[i]
     return c
 
+# Calculates the objective function of a solution
 def objective(trips):
-    obj=0
+    obj = 0
     for trip in trips:
-        obj+=cost(trip)
+        obj += cost(trip)
     return obj
+
+# For each vehicle, it finds the complete route from a specific path and its cost
+def calculate(N,K,Q,q,d,seq,path):
+    trips,delivs,costs = [], [], []
+    curr = 1
+    nxt = path[curr]
+    i = 1
+    while i < N:
+        trip = [0]
+        deliv = [0]
+        while i < N and seq[i] != nxt:
+            trip.append(seq[i])
+            deliv.append(q[seq[i]])
+            i+=1
+        trip.append(seq[i]), trip.append(0)
+        deliv.append(q[seq[i]]), deliv.append(0)
+        trips.append(trip)
+        delivs.append(deliv)
+        costs.append(cost(trip, deliv, d))
+        i += 1
+        curr += 1
+        if(curr>K):
+            break
+        nxt = path[curr]
+    return trips, delivs, costs
--- Split.py
+++ Split.py
@@ -1,5 +1,6 @@
+from queue import Queue
+from Function import *
 import math
-import numpy as np
 
 #N: nodes
 #K: vehicles
@@ -7,34 +8,100 @@ import numpy as np
 #q: demands
 #d: distances
 
-def split(N,K,Q,q,d,seq):
-    v=[math.inf]*N
-    v[0]=0
-    w=[math.inf]*N
-    w[0]=0
-    p=[[0]*K]*N
+# Constructs the auxiliary graph for the split algorithm
+    # Arc (i,j) means that there is a path from node i+1 to node j (including the depot)
+def aux(N, Q, q, d, seq):
+    g = [None]*N
+    for i in range(N):
+        gi = []
+        load = Q
+        dist = 0
+        cost = 0
+        j = i
+        while j < N-1 and load > 0:
+            if i == j:
+                dist += d[0][seq[j+1]]
+            else:
+                dist += d[seq[j]][seq[j+1]]
+            cost += dist * q[seq[j+1]]
+            load -= q[seq[j+1]]
+            if load >= 0:
+                gi.append((seq[j+1],cost))
+            else:
+                break
+            j += 1
+        g[seq[i]]=gi
+    return g
 
-    for k in range(K):
-        for i in range(N-1):
-            j=i
-            load=q[seq[i]]
-            dist=0
-            cost=0
-            while(j<N-1 and load<=Q):
-                if(i==j):
-                    dist=d[seq[0]][seq[j+1]]
-                    cost=dist*q[seq[j+1]]
-                else:
-                    dist+=d[seq[j]][seq[j+1]]
-                    cost+=dist*q[seq[j+1]]
-                if(w[j-1]+cost<v[j]):
-                    v[j]=w[i-1]+cost
-                    p[j][k]=i-1
-                j+=1
-                load+=q[seq[j]]
-                print("load:",load)
-                print("dist:",dist)
-                print("cost:",cost)
+# Reconstructs the path using exactly K arcs of the auxiliary graph (K vehicles)
+def find_path(K, end, p):
+    path = []
+    while end!=-1:
+        path.append(end)
+        end = p[end][K]
+        K -= 1
+    path.reverse()
+    return path
 
-        for i in range(N):
-            w[i]=v[i]
+# Splits the sequence into K routes using the auxiliary graph and DP
+def split(N, K, Q, q, d, seq):
+    g=aux(N, Q, q, d, seq)
+    in_degree = [0] * N
+    p = [[-1 for _ in range(N)] for _ in range(N)]
+
+    for i in range(N):
+        for v in g[i]:
+            in_degree[v[0]] += 1
+    q = Queue()
+    q.put(0)
+    in_degree[0] = -1
+    dist = [[math.inf for _ in range(N)] for _ in range(N)]
+    dist[0][0] = 0
+
+    while not q.empty():
+        u = q.get()
+        for v in g[u]:
+            in_degree[v[0]] -= 1
+            if in_degree[v[0]] == 0:
+                in_degree[v[0]] = -1
+                q.put(v[0])
+            for i in range(1, N):
+                if dist[v[0]][i] > dist[u][i - 1] + v[1]:
+                    dist[v[0]][i] = dist[u][i - 1] + v[1]
+                    p[v[0]][i] = u
+    
+    path = find_path(K, seq[N - 1], p)
+    return path
+
+
+# def split(N,K,Q,q,d,seq):
+#     v=[math.inf]*N
+#     v[0]=0
+#     w=[math.inf]*N
+#     w[0]=0
+#     p=[[0]*K]*N
+
+#     for k in range(K):
+#         for i in range(N-1):
+#             j=i
+#             load=q[seq[i]]
+#             dist=0
+#             cost=0
+#             while(j<N-1 and load<=Q):
+#                 if(i==j):
+#                     dist=d[seq[0]][seq[j+1]]
+#                     cost=dist*q[seq[j+1]]
+#                 else:
+#                     dist+=d[seq[j]][seq[j+1]]
+#                     cost+=dist*q[seq[j+1]]
+#                 if(w[j-1]+cost<v[j]):
+#                     v[j]=w[i-1]+cost
+#                     p[j][k]=i-1
+#                 j+=1
+#                 load+=q[seq[j]]
+#                 # print("load:",load)
+#                 # print("dist:",dist)
+#                 # print("cost:",cost)
+
+#         for i in range(N):
+#             w[i]=v[i]
